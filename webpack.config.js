f4e2366e1432f11b96d1bb6aed1a4e079ccaab43
Abdul Khan <abdul.k.khan75@gmail.com>
2018-11-20 15:26:50 -0500

    sogn

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 
-    entry: path.join(__dirname, 'src/lib/index.js'),
+    entry: path.join(__dirname, './src/lib/index.js'),
 
     output: {
         path: path.resolve(__dirname, './dist/lib'),
