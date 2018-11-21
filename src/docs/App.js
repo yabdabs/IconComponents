ca9c1a6df906fc23b84dbf714d0d0d5c1c82f747
Abdul Khan <abdul.k.khan75@gmail.com>
2018-11-21 14:17:07 -0500

    Delete App.js

--- src/docs/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-import TestComponent from './TestComponent.js';
-
-const App = () => (
-    <TestComponent />
-);
-render(<App />, document.getElementById("root"));
