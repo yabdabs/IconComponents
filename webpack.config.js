9bc9b78f7365dce1352410e7b6bb8543c5bcc48b
Abdul Khan <abdul.k.khan75@gmail.com>
2018-11-21 14:19:35 -0500

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+    entry: path.join(__dirname, './src/lib/index.js'),
+
+    output: {
+        path: path.resolve(__dirname, './dist/lib'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs'
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/docs/index.html',
+            filename: "./index.html"
+        })
+    ]
+
+}
