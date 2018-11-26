b719b951e95b740467571d74b067bc71b42babde
Abdul Khan <abdul.k.khan75@gmail.com>
2018-11-26 08:12:41 -0500

    export svgs with correct naming

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,182 +1,182 @@
-import AccordionCollapseLarge from './IconComponents/AccordionCollapseLarge.js'
-import AccordionCollapseSmall from './IconComponents/AccordionCollapseSmall.js'
-import AccordionExpandLarge from './IconComponents/AccordionExpandLarge.js'
-import AccordionExpandSmall from './IconComponents/AccordionExpandSmall.js'
-import AccountSettings from './IconComponents/AccountSettings.js'
-import AndroidFingerprintAuth from './IconComponents/AndroidFingerprintAuth.js'
-import AndroidFingerprintAuthSuccess from './IconComponents/AndroidFingerprintAuthSuccess.js'
-import AndroidHidePassword from './IconComponents/AndroidHidePassword.js'
-import AndroidShowPassword from './IconComponents/AndroidShowPassword.js'
-import ArrowAngleDown from './IconComponents/ArrowAngleDown.js'
-import ArrowAngleLeft from './IconComponents/ArrowAngleLeft.js'
-import ArrowAngleLeft1 from './IconComponents/ArrowAngleLeft1.js'
-import ArrowAngleRight from './IconComponents/ArrowAngleRight.js'
-import ArrowAngleUp from './IconComponents/ArrowAngleUp.js'
-import ArrowLeft from './IconComponents/ArrowLeft.js'
-import ArrowLeft1 from './IconComponents/ArrowLeft1.js'
-import ArrowRight from './IconComponents/ArrowRight.js'
-import Calendar from './IconComponents/Calendar.js'
-import Calendar1 from './IconComponents/Calendar1.js'
-import CaretDown from './IconComponents/CaretDown.js'
-import CaretLeft from './IconComponents/CaretLeft.js'
-import CaretRight from './IconComponents/CaretRight.js'
-import CaretUp from './IconComponents/CaretUp.js'
-import ClearX from './IconComponents/ClearX.js'
-import Clock from './IconComponents/Clock.js'
-import CloseX from './IconComponents/CloseX.js'
-import ExternalLink from './IconComponents/ExternalLink.js'
-import FlagCanada from './IconComponents/FlagCanada.js'
-import FlagMexico from './IconComponents/FlagMexico.js'
-import FlagUsa from './IconComponents/FlagUsa.js'
-import HeaderBackArrow from './IconComponents/HeaderBackArrow.js'
-import HeaderBackArrow1 from './IconComponents/HeaderBackArrow1.js'
-import HeaderBackArrow2 from './IconComponents/HeaderBackArrow2.js'
-import HeaderBackArrow3 from './IconComponents/HeaderBackArrow3.js'
-import HeaderBackArrow4 from './IconComponents/HeaderBackArrow4.js'
-import HelpLight from './IconComponents/HelpLight.js'
-import Icon from './IconComponents/Icon.js'
-import IOsBilling from './IconComponents/IOsBilling.js'
-import IOsHome from './IconComponents/IOsHome.js'
-import IOsOrders from './IconComponents/IOsOrders.js'
-import IOsPrescriptions from './IconComponents/IOsPrescriptions.js'
-import IOsSettings from './IconComponents/IOsSettings.js'
-import IOsSupport from './IconComponents/IOsSupport.js'
-import IOsTouchIdAccredo from './IconComponents/IOsTouchIdAccredo.js'
-import IOsTouchIdAccredo1 from './IconComponents/IOsTouchIdAccredo1.js'
-import IOsTouchIdAccredo2 from './IconComponents/IOsTouchIdAccredo2.js'
-import IOsTouchIdAccredo3 from './IconComponents/IOsTouchIdAccredo3.js'
-import IOsTouchIdEsi from './IconComponents/IOsTouchIdEsi.js'
-import IOsTouchIdEsi1 from './IconComponents/IOsTouchIdEsi1.js'
-import IOsTouchIdEsi2 from './IconComponents/IOsTouchIdEsi2.js'
-import IOsTouchIdEsi3 from './IconComponents/IOsTouchIdEsi3.js'
-import IOsTouchIdSuccess from './IconComponents/IOsTouchIdSuccess.js'
-import LanguageGlobe from './IconComponents/LanguageGlobe.js'
-import Med from './IconComponents/Med.js'
-import Med1 from './IconComponents/Med1.js'
-import MedGeneralRx from './IconComponents/MedGeneralRx.js'
-import MedSupplyBox from './IconComponents/MedSupplyBox.js'
-import Menu from './IconComponents/Menu.js'
-import Menu1 from './IconComponents/Menu1.js'
-import Menu2 from './IconComponents/Menu2.js'
-import Menu3 from './IconComponents/Menu3.js'
-import MessageAttachment from './IconComponents/MessageAttachment.js'
-import MessageCenter from './IconComponents/MessageCenter.js'
-import MessageRead from './IconComponents/MessageRead.js'
-import MessageReply from './IconComponents/MessageReply.js'
-import Messages from './IconComponents/Messages.js'
-import MessageUnread from './IconComponents/MessageUnread.js'
-import NotificationCareDark from './IconComponents/NotificationCareDark.js'
-import NotificationCareLight from './IconComponents/NotificationCareLight.js'
-import NotificationEmergency from './IconComponents/NotificationEmergency.js'
-import NotificationEmergencyWhite from './IconComponents/NotificationEmergencyWhite.js'
-import NotificationErrorDark from './IconComponents/NotificationErrorDark.js'
-import NotificationErrorLight from './IconComponents/NotificationErrorLight.js'
-import NotificationHelpDark from './IconComponents/NotificationHelpDark.js'
-import NotificationHelpLight from './IconComponents/NotificationHelpLight.js'
-import NotificationHelpSmallInline from './IconComponents/NotificationHelpSmallInline.js'
-import NotificationInfoDark from './IconComponents/NotificationInfoDark.js'
-import NotificationInfoLight from './IconComponents/NotificationInfoLight.js'
-import NotificationInfoOtherBg from './IconComponents/NotificationInfoOtherBg.js'
-import NotificationInfoSmallInline from './IconComponents/NotificationInfoSmallInline.js'
-import NotificationSuccessDark from './IconComponents/NotificationSuccessDark.js'
-import NotificationSuccessLight from './IconComponents/NotificationSuccessLight.js'
-import NotificationWarningDark from './IconComponents/NotificationWarningDark.js'
-import NotificationWarningLight from './IconComponents/NotificationWarningLight.js'
-import Print from './IconComponents/Print.js'
-import Rectangle from './IconComponents/Rectangle.js'
-import Search from './IconComponents/Search.js'
-import Search1 from './IconComponents/Search1.js'
-import Search2 from './IconComponents/Search2.js'
+import SvgAccordionCollapseLarge from './IconComponents/AccordionCollapseLarge.js'
+import SvgAccordionCollapseSmall from './IconComponents/AccordionCollapseSmall.js'
+import SvgAccordionExpandLarge from './IconComponents/AccordionExpandLarge.js'
+import SvgAccordionExpandSmall from './IconComponents/AccordionExpandSmall.js'
+import SvgAccountSettings from './IconComponents/AccountSettings.js'
+import SvgAndroidFingerprintAuth from './IconComponents/AndroidFingerprintAuth.js'
+import SvgAndroidFingerprintAuthSuccess from './IconComponents/AndroidFingerprintAuthSuccess.js'
+import SvgAndroidHidePassword from './IconComponents/AndroidHidePassword.js'
+import SvgAndroidShowPassword from './IconComponents/AndroidShowPassword.js'
+import SvgArrowAngleDown from './IconComponents/ArrowAngleDown.js'
+import SvgArrowAngleLeft from './IconComponents/ArrowAngleLeft.js'
+import SvgArrowAngleLeft1 from './IconComponents/ArrowAngleLeft1.js'
+import SvgArrowAngleRight from './IconComponents/ArrowAngleRight.js'
+import SvgArrowAngleUp from './IconComponents/ArrowAngleUp.js'
+import SvgArrowLeft from './IconComponents/ArrowLeft.js'
+import SvgArrowLeft1 from './IconComponents/ArrowLeft1.js'
+import SvgArrowRight from './IconComponents/ArrowRight.js'
+import SvgCalendar from './IconComponents/Calendar.js'
+import SvgCalendar1 from './IconComponents/Calendar1.js'
+import SvgCaretDown from './IconComponents/CaretDown.js'
+import SvgCaretLeft from './IconComponents/CaretLeft.js'
+import SvgCaretRight from './IconComponents/CaretRight.js'
+import SvgCaretUp from './IconComponents/CaretUp.js'
+import SvgClearX from './IconComponents/ClearX.js'
+import SvgClock from './IconComponents/Clock.js'
+import SvgCloseX from './IconComponents/CloseX.js'
+import SvgExternalLink from './IconComponents/ExternalLink.js'
+import SvgFlagCanada from './IconComponents/FlagCanada.js'
+import SvgFlagMexico from './IconComponents/FlagMexico.js'
+import SvgFlagUsa from './IconComponents/FlagUsa.js'
+import SvgHeaderBackArrow from './IconComponents/HeaderBackArrow.js'
+import SvgHeaderBackArrow1 from './IconComponents/HeaderBackArrow1.js'
+import SvgHeaderBackArrow2 from './IconComponents/HeaderBackArrow2.js'
+import SvgHeaderBackArrow3 from './IconComponents/HeaderBackArrow3.js'
+import SvgHeaderBackArrow4 from './IconComponents/HeaderBackArrow4.js'
+import SvgHelpLight from './IconComponents/HelpLight.js'
+import SvgIcon from './IconComponents/Icon.js'
+import SvgIOsBilling from './IconComponents/IOsBilling.js'
+import SvgIOsHome from './IconComponents/IOsHome.js'
+import SvgIOsOrders from './IconComponents/IOsOrders.js'
+import SvgIOsPrescriptions from './IconComponents/IOsPrescriptions.js'
+import SvgIOsSettings from './IconComponents/IOsSettings.js'
+import SvgIOsSupport from './IconComponents/IOsSupport.js'
+import SvgIOsTouchIdAccredo from './IconComponents/IOsTouchIdAccredo.js'
+import SvgIOsTouchIdAccredo1 from './IconComponents/IOsTouchIdAccredo1.js'
+import SvgIOsTouchIdAccredo2 from './IconComponents/IOsTouchIdAccredo2.js'
+import SvgIOsTouchIdAccredo3 from './IconComponents/IOsTouchIdAccredo3.js'
+import SvgIOsTouchIdEsi from './IconComponents/IOsTouchIdEsi.js'
+import SvgIOsTouchIdEsi1 from './IconComponents/IOsTouchIdEsi1.js'
+import SvgIOsTouchIdEsi2 from './IconComponents/IOsTouchIdEsi2.js'
+import SvgIOsTouchIdEsi3 from './IconComponents/IOsTouchIdEsi3.js'
+import SvgIOsTouchIdSuccess from './IconComponents/IOsTouchIdSuccess.js'
+import SvgLanguageGlobe from './IconComponents/LanguageGlobe.js'
+import SvgMed from './IconComponents/Med.js'
+import SvgMed1 from './IconComponents/Med1.js'
+import SvgMedGeneralRx from './IconComponents/MedGeneralRx.js'
+import SvgMedSupplyBox from './IconComponents/MedSupplyBox.js'
+import SvgMenu from './IconComponents/Menu.js'
+import SvgMenu1 from './IconComponents/Menu1.js'
+import SvgMenu2 from './IconComponents/Menu2.js'
+import SvgMenu3 from './IconComponents/Menu3.js'
+import SvgMessageAttachment from './IconComponents/MessageAttachment.js'
+import SvgMessageCenter from './IconComponents/MessageCenter.js'
+import SvgMessageRead from './IconComponents/MessageRead.js'
+import SvgMessageReply from './IconComponents/MessageReply.js'
+import SvgMessages from './IconComponents/Messages.js'
+import SvgMessageUnread from './IconComponents/MessageUnread.js'
+import SvgNotificationCareDark from './IconComponents/NotificationCareDark.js'
+import SvgNotificationCareLight from './IconComponents/NotificationCareLight.js'
+import SvgNotificationEmergency from './IconComponents/NotificationEmergency.js'
+import SvgNotificationEmergencyWhite from './IconComponents/NotificationEmergencyWhite.js'
+import SvgNotificationErrorDark from './IconComponents/NotificationErrorDark.js'
+import SvgNotificationErrorLight from './IconComponents/NotificationErrorLight.js'
+import SvgNotificationHelpDark from './IconComponents/NotificationHelpDark.js'
+import SvgNotificationHelpLight from './IconComponents/NotificationHelpLight.js'
+import SvgNotificationHelpSmallInline from './IconComponents/NotificationHelpSmallInline.js'
+import SvgNotificationInfoDark from './IconComponents/NotificationInfoDark.js'
+import SvgNotificationInfoLight from './IconComponents/NotificationInfoLight.js'
+import SvgNotificationInfoOtherBg from './IconComponents/NotificationInfoOtherBg.js'
+import SvgNotificationInfoSmallInline from './IconComponents/NotificationInfoSmallInline.js'
+import SvgNotificationSuccessDark from './IconComponents/NotificationSuccessDark.js'
+import SvgNotificationSuccessLight from './IconComponents/NotificationSuccessLight.js'
+import SvgNotificationWarningDark from './IconComponents/NotificationWarningDark.js'
+import SvgNotificationWarningLight from './IconComponents/NotificationWarningLight.js'
+import SvgPrint from './IconComponents/Print.js'
+import SvgRectangle from './IconComponents/Rectangle.js'
+import SvgSearch from './IconComponents/Search.js'
+import SvgSearch1 from './IconComponents/Search1.js'
+import SvgSearch2 from './IconComponents/Search2.js'
 
 export {
-    AccordionCollapseLarge,
-    AccordionCollapseSmall,
-    AccordionExpandLarge,
-    AccordionExpandSmall,
-    AccountSettings,
-    AndroidFingerprintAuth,
-    AndroidFingerprintAuthSuccess,
-    AndroidHidePassword,
-    AndroidShowPassword,
-    ArrowAngleDown,
-    ArrowAngleLeft,
-    ArrowAngleLeft1,
-    ArrowAngleRight,
-    ArrowAngleUp,
-    ArrowLeft,
-    ArrowLeft1,
-    ArrowRight,
-    Calendar,
-    Calendar1,
-    CaretDown,
-    CaretLeft,
-    CaretRight,
-    CaretUp,
-    ClearX,
-    Clock,
-    CloseX,
-    ExternalLink,
-    FlagCanada,
-    FlagMexico,
-    FlagUsa,
-    HeaderBackArrow,
-    HeaderBackArrow1,
-    HeaderBackArrow2,
-    HeaderBackArrow3,
-    HeaderBackArrow4,
-    HelpLight,
-    Icon,
-    IOsBilling,
-    IOsHome,
-    IOsOrders,
-    IOsPrescriptions,
-    IOsSettings,
-    IOsSupport,
-    IOsTouchIdAccredo,
-    IOsTouchIdAccredo1,
-    IOsTouchIdAccredo2,
-    IOsTouchIdAccredo3,
-    IOsTouchIdEsi,
-    IOsTouchIdEsi1,
-    IOsTouchIdEsi2,
-    IOsTouchIdEsi3,
-    IOsTouchIdSuccess,
-    LanguageGlobe,
-    Med,
-    Med1,
-    MedGeneralRx,
-    MedSupplyBox,
-    Menu,
-    Menu1,
-    Menu2,
-    Menu3,
-    MessageAttachment,
-    MessageCenter,
-    MessageRead,
-    MessageReply,
-    Messages,
-    MessageUnread,
-    NotificationCareDark,
-    NotificationCareLight,
-    NotificationEmergency,
-    NotificationEmergencyWhite,
-    NotificationErrorDark,
-    NotificationErrorLight,
-    NotificationHelpDark,
-    NotificationHelpLight,
-    NotificationHelpSmallInline,
-    NotificationInfoDark,
-    NotificationInfoLight,
-    NotificationInfoOtherBg,
-    NotificationInfoSmallInline,
-    NotificationSuccessDark,
-    NotificationSuccessLight,
-    NotificationWarningDark,
-    NotificationWarningLight,
-    Print,
-    Rectangle,
-    Search,
-    Search1,
-    Search2
+    SvgAccordionCollapseLarge,
+    SvgAccordionCollapseSmall,
+    SvgAccordionExpandLarge,
+    SvgAccordionExpandSmall,
+    SvgAccountSettings,
+    SvgAndroidFingerprintAuth,
+    SvgAndroidFingerprintAuthSuccess,
+    SvgAndroidHidePassword,
+    SvgAndroidShowPassword,
+    SvgArrowAngleDown,
+    SvgArrowAngleLeft,
+    SvgArrowAngleLeft1,
+    SvgArrowAngleRight,
+    SvgArrowAngleUp,
+    SvgArrowLeft,
+    SvgArrowLeft1,
+    SvgArrowRight,
+    SvgCalendar,
+    SvgCalendar1,
+    SvgCaretDown,
+    SvgCaretLeft,
+    SvgCaretRight,
+    SvgCaretUp,
+    SvgClearX,
+    SvgClock,
+    SvgCloseX,
+    SvgExternalLink,
+    SvgFlagCanada,
+    SvgFlagMexico,
+    SvgFlagUsa,
+    SvgHeaderBackArrow,
+    SvgHeaderBackArrow1,
+    SvgHeaderBackArrow2,
+    SvgHeaderBackArrow3,
+    SvgHeaderBackArrow4,
+    SvgHelpLight,
+    SvgIcon,
+    SvgIOsBilling,
+    SvgIOsHome,
+    SvgIOsOrders,
+    SvgIOsPrescriptions,
+    SvgIOsSettings,
+    SvgIOsSupport,
+    SvgIOsTouchIdAccredo,
+    SvgIOsTouchIdAccredo1,
+    SvgIOsTouchIdAccredo2,
+    SvgIOsTouchIdAccredo3,
+    SvgIOsTouchIdEsi,
+    SvgIOsTouchIdEsi1,
+    SvgIOsTouchIdEsi2,
+    SvgIOsTouchIdEsi3,
+    SvgIOsTouchIdSuccess,
+    SvgLanguageGlobe,
+    SvgMed,
+    SvgMed1,
+    SvgMedGeneralRx,
+    SvgMedSupplyBox,
+    SvgMenu,
+    SvgMenu1,
+    SvgMenu2,
+    SvgMenu3,
+    SvgMessageAttachment,
+    SvgMessageCenter,
+    SvgMessageRead,
+    SvgMessageReply,
+    SvgMessages,
+    SvgMessageUnread,
+    SvgNotificationCareDark,
+    SvgNotificationCareLight,
+    SvgNotificationEmergency,
+    SvgNotificationEmergencyWhite,
+    SvgNotificationErrorDark,
+    SvgNotificationErrorLight,
+    SvgNotificationHelpDark,
+    SvgNotificationHelpLight,
+    SvgNotificationHelpSmallInline,
+    SvgNotificationInfoDark,
+    SvgNotificationInfoLight,
+    SvgNotificationInfoOtherBg,
+    SvgNotificationInfoSmallInline,
+    SvgNotificationSuccessDark,
+    SvgNotificationSuccessLight,
+    SvgNotificationWarningDark,
+    SvgNotificationWarningLight,
+    SvgPrint,
+    SvgRectangle,
+    SvgSearch,
+    SvgSearch1,
+    SvgSearch2
 }
 
